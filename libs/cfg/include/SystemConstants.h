d71a6cf2da5e627760489ffda49c4bdca2be1b25
Khizar Ali Shah <khizaralishahcs07@gmail.com>
2026-06-11 11:48:45 +0500

    Add STM flags

--- libs/cfg/include/SystemConstants.h
+++ libs/cfg/include/SystemConstants.h
@@ -2,6 +2,8 @@
 #define SYSTEM_CONSTANTS
 
 #include <utility>
+#include <cstdint>
+#include <array>
 
 struct SystemConstants {
   static constexpr float PLAYER_RADIUS = 18.5f;
@@ -15,6 +17,18 @@ struct SystemConstants {
   static constexpr unsigned int SCREEN_HEIGHT = 580;
 
   static constexpr float OBSTACLE_TOLERANCE = 30.0f;
+};
+
+struct NetworkConfig {
+  // IPs (192.168.1.XYZ)
+  static constexpr uint8_t base_station_ip = 100;
+  static constexpr uint8_t host_ip = 101;
+
+  // Ports
+  static constexpr uint16_t host_listen_port = 8080;
+
+  // Message sizes
+  static constexpr int udp_buffer_max_size = 1024;
 };
 
 #endif  // SYSTEM_CONSTANTS
