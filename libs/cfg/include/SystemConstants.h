20f267aff2413c0c8a2ba6b307d52f1877d96d38
Khizar Ali Shah <khizaralishahcs07@gmail.com>
2026-06-15 15:25:18 +0500

    Change network time slots

--- libs/cfg/include/SystemConstants.h
+++ libs/cfg/include/SystemConstants.h
@@ -6,6 +6,10 @@
 #include <utility>
 
 struct SystemConstants {
+  static constexpr uint8_t num_robots = 6;
+
+  static_assert(num_robots >= 1 && num_robots <= 6 && "Check [SystemConstants]: num_robots\r\n");
+  
   static constexpr unsigned int SCREEN_WIDTH = 950;
   static constexpr unsigned int SCREEN_HEIGHT = 580;
 };
