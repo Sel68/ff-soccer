a8e6aad541d73478ee892cd4197c747248e97164
Sel68 <saadhasanellahie@gmail.com>
2026-06-23 11:09:38 +0500

    fix: transmit wheel param

--- libs/handler/include/Transmitter.h
+++ libs/handler/include/Transmitter.h
@@ -19,7 +19,7 @@ struct RobotTelemetryPacket {
   float vel_x;
   float vel_y;
   float vel_theta;
-  float wheel_speeds[4];
+  float wheel_speeds[SystemConstants::num_wheels];
 };
 #pragma pack(pop)
 
@@ -33,7 +33,7 @@ class Transmitter {
 
   // didnt  make it static to allow multi transmissions
   void transmit(int robot_id, double px, double py, double ptheta, const ChassisVelocity& vel,
-                const Eigen::VectorXd& wheels);
+                const std::array<double, SystemConstants::num_wheels>& wheels);
 };
 
 #endif
