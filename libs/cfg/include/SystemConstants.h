a4c27904a5a984624d23ec24be851efece7314b1
Sel68 <saadhasanellahie@gmail.com>
2026-06-23 16:54:53 +0500

    add: assert for num of wheels

--- libs/cfg/include/SystemConstants.h
+++ libs/cfg/include/SystemConstants.h
@@ -10,6 +10,7 @@ struct SystemConstants {
   static constexpr uint8_t num_wheels = 4;
 
   static_assert(num_robots >= 1 && num_robots <= 6 && "Check [SystemConstants]: num_robots\r\n");
+  static_assert(num_wheels == 4 && "Check [SystemConstants]: num_wheels\r\n");
 
   static constexpr unsigned int SCREEN_WIDTH = 950;
   static constexpr unsigned int SCREEN_HEIGHT = 580;
