41af7ebc3e0a71acc74542b597594646bacdf4c8
Khizar Ali Shah <khizaralishahcs07@gmail.com>
2026-07-22 09:02:51 +0500

    Re-add colors in logger

--- libs/debug/include/Logger.h
+++ libs/debug/include/Logger.h
@@ -6,6 +6,11 @@
 #include "Manipulators.h"
 #include "LoggerPC.h"
 
+#define COLOR_RED "\033[0;31m"
+#define COLOR_BLUE "\033[0;34m"
+#define COLOR_GREEN "\033[0;32m"
+#define COLOR_RESET "\033[0m"
+
 #ifdef LOG_NO_OP  // from cmake
 #include "LoggerNoOp.h"
 #endif
